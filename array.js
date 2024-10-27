2b130804a1477149123d56aec1061eb6a96e4acc
Md Habibullah <smrakib333@gmail.com>
2024-10-27 08:35:57 +0600

    first problem solved

---
+++ array.js
@@ -0,0 +1,11 @@
+/* Task 1
+1. Declare an array
+a.Declare an array with 5 elements containing fruits
+b.console log the 3rd index element
+c.change the value of the 2nd index element to jambura
+d.console log the final array
+*/
+const fruits = ["apple", "banana", "jackfruit", "strawberry", "lichi"];
+console.log("#rd index of fruits is: ", fruits[2]);
+fruits[1] = "jambura";
+console.log("Final array is: ", fruits);
