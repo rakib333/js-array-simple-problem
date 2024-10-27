7b042b96673d3e733d920841c0c460be05bcc44b
Md Habibullah <smrakib333@gmail.com>
2024-10-27 08:45:58 +0600

    2nd pblm solved

--- array.js
+++ array.js
@@ -9,3 +9,27 @@ const fruits = ["apple", "banana", "jackfruit", "strawberry", "lichi"];
 console.log("#rd index of fruits is: ", fruits[2]);
 fruits[1] = "jambura";
 console.log("Final array is: ", fruits);
+
+/*Task 2
+2. Add or remove elements
+a.Declare an array of 3 tourist destinations
+b.Add a new tourist destination to your tourist array
+c.Add two more to your array
+d.Remove the last tourist destination you have added
+e.display the final array as output
+*/
+
+//a.Declare an array of 3 tourist destinations
+const touristsDestination = ["Bandorban", "cox's bazar", "Rangamati"];
+
+//b.Add a new tourist destination to your tourist array
+touristsDestination.push("Nikli Haor");
+
+//c.Add two more to your array
+touristsDestination.push("Sunamgonj", "Kanchonjonga");
+
+// console.log(touristsDestination);
+
+//d.Remove the last tourist destination you have added
+touristsDestination.pop();
+console.log("Final array is: ", touristsDestination);
