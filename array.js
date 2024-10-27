7ce4394c986f82df5afd1b0682e2c7f0dbcab898
Md Habibullah <smrakib333@gmail.com>
2024-10-27 09:04:21 +0600

    5th  pblm solved

--- array.js
+++ array.js
@@ -77,3 +77,22 @@ console.log(
   "It is a :",
   typeof name
 );
+
+/*
+Task 5
+5. Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+
+*/
+
+const numbers1 = [1, 2, 3, 4, 5];
+const numbers2 = [6, 7, 8, 9, 10];
+console.log(numbers1);
+console.log(numbers2);
+
+const concatedArray = numbers1.concat(numbers2);
+console.log(concatedArray);
